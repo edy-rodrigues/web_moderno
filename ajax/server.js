cc6657095d7eced70b99c5e183f84e93738cfce8
= <=>
2018-08-22 18:18:00 -0300

    Inserido 22/08/2018 18:17

---
+++ ajax/server.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/teste', (req, res) => res.send("teste"))
+app.listen(3000, () => console.log('Executando'))
